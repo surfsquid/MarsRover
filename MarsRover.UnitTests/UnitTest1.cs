bc813dc7c9ceab7f5a60964331405fffcffc174e
surfsquid <surfsquid@gmail.com>
2016-03-09 21:53:15 +0000

    added unit test

---
+++ MarsRover.UnitTests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRover.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRoverMovement()
+        {
+
+            //Test Input:
+            //5 5
+            //1 2 N
+            //LMLMLMLMM
+            //3 3 E
+            //MMRMMRMRRM
+            //Expected Output:
+            //1 3 N
+            //5 1 E
+
+            // Arrange
+            Rover testRover = new Rover();
+            testRover.Initialize("1 2 N", "LMLMLMLMM");
+            // Act
+            testRover.Move();
+            // Assert
+            Assert.AreEqual("1 3 N", testRover.GetLocation());
+        }
+    }
+}
